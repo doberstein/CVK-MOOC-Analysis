89993dcb79ff6f4311aa4a1ab9daf162dedee73e
doberstein <dorian.doberstein@unitybox.de>
2018-12-14 16:03:00 +0100

    initial commit

---
+++ base_64_encoding.R
@@ -0,0 +1,20 @@
+
+
+all_groups_and_users[all_groups_and_users$user_id == "hMkZlwNRkZL6tyz+Fn+sFQ==",]
+
+all_groups_and_users[all_groups_and_users$user_id == "ouFVSEQq09GEg6Z/713Cdw==",]
+
+# LINK / QUESTION IDs
+l1 <- "aE1rWmx3TlJrWkw2dHl6K0ZuK3NGUT09"
+l2 <- "aG85SC9UR0R1TStLK2t1em9VRHpQQT09"
+l3 <- "aGxHWUx1VWpOSDhBNHdqNUQzOWF1UT09"
+l4 <- "b0d6SVJ4NDZnZ0dmdWZqcktIV2tNZz09"
+l5 <- "b3VGVlNFUXEwOUdFZzZaLzcxM0Nkdz09"
+
+# LRS / BACKEND USER IDs
+
+u1 <- "hMkZlwNRkZL6tyz+Fn+sFQ=="
+u2 <- "ouFVSEQq09GEg6Z/713Cdw=="
+
+b_u1 <- base64_enc(u1)
+b_u1 == l1
